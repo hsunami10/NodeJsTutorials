c4840be2b027eaa4be15d8338ff9d48207f5eba1
Michael Hsu <michael100.hsu@gmail.com>
2016-11-20 22:03:17 -0600

    modules & require : second tutorial

---
+++ app.js
@@ -0,0 +1,5 @@
+// require() is a function that takes in a string that contains the location of the module to import
+
+// require returns module.exports/what is exposed
+var greet = require("./greet");
+greet():
--- greet.js
+++ greet.js
@@ -1,4 +1,11 @@
 // First module
 // A module is a part of a larger program
 
-console.log("Hello");
+// Code inside module is protected, so variables and functions not in the scope of other programs
+// ***BUT*** to allow other programs to expose objects...
+module.exports = greet;
+
+var greet = function() {
+    console.log("Hello!");
+};
+greet();
